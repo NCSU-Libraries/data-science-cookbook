eb5ca73d9e858f1385775c10b3bbcf1f6b6efd92
Rong <56700201+rhuang44@users.noreply.github.com>
2021-12-03 15:43:29 -0500

    Update FB_stock.py

--- python/bokeh/FB_stock.py
+++ python/bokeh/FB_stock.py
@@ -1,10 +1,3 @@
-#-------------------------------------------------------------------
-# TITLE: Interactive Visualization of FB Stock using Bokeh package
-# Code reference: https://github.com/bokeh/bokeh/tree/branch-3.0/examples/app/movies
-# Dateset: https://www.kaggle.com/kaushiksuresh147/faang-fbamazonapplenetflixgoogle-stocks?select=FB+Historical+Data.csv
-# To excute: bokeh serve --show FB_stock.py
-#---------------------------------------------------------------------
-
 import pandas as pd
 import numpy as np
 from datetime import date, datetime
